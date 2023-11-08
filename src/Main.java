0611e45507f7d0a7c3b1e50d60d0b3e08a8cbc88
SvetlanaLukyano <gaechkadale@yandex.ru>
2023-11-08 15:56:59 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,6 @@ public class Main {
         // IntelliJ IDEA suggests fixing it.
         System.out.println("Hello world!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-//        for (int i = 1; i <= 5; i++) {
-//
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
 
         }
     }
